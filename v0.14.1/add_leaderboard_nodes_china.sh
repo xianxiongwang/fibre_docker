83da9fcfce23fbce96e01dad73bfb88f1d4dbdad
xianxiongwang <171279316@qq.com>
2018-01-16 11:56:19 +0800

    add header nodes

--- v0.14.1/add_leaderboard_nodes_china.sh
+++ v0.14.1/add_leaderboard_nodes_china.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# add leaderboard nodes to bitcoind, ip list is from bitnodes.21.co
+# add leaderboard nodes to bitcoind, ip list is from bitnodes.earn.com
 #
-# @copyright btc.com
-# @author Zhibiao Pan
-# @since 2016-08
+# @copyright xhash.com
+# @author Xianxiong Wang
+# @since 2018-01
 #
 
-curl "https://bitnodes.21.co/nodes/leaderboard/?q=china" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort -n | uniq | xargs -I{} bitcoin-cli addnode "{}" "onetry"
+curl "https://bitnodes.earn.com/nodes/leaderboard/?q=china" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort -n | uniq | xargs -I{} bitcoin-cli addnode "{}" "onetry"
